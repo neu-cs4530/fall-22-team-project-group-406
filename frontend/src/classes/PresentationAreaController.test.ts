24765a02cc8156ffc3a0df7877b95683e85ff74b
Kevin Cam <kevinsebcam@gmail.com>
2022-11-26 12:35:39 -0500

    Test 'isEmpty' and the title property of PresentationAreaController class.

--- frontend/src/classes/PresentationAreaController.test.ts
+++ frontend/src/classes/PresentationAreaController.test.ts
@@ -24,9 +24,41 @@ describe('DocumentAreaController', () => {
     mockClear(mockListeners.slideChange);
     mockClear(mockListeners.occupantsChange);
     mockClear(mockListeners.documentChange);
+    mockClear(mockListeners.titleChange);
     testArea.addListener('slideChange', mockListeners.slideChange);
     testArea.addListener('occupantsChange', mockListeners.occupantsChange);
     testArea.addListener('documentChange', mockListeners.documentChange);
+    testArea.addListener('titleChange', mockListeners.titleChange);
+  });
+  describe('isEmpty', () => {
+    it('Returns true if the occupants list is empty', () => {
+      testArea.occupants = [];
+      expect(testArea.isEmpty()).toBe(true);
+    });
+    it('Returns true if the title is undefined', () => {
+      testArea.title = undefined;
+      expect(testArea.isEmpty()).toBe(true);
+    });
+    it('Returns true if the document is undefined', () => {
+      testArea.document = undefined;
+      expect(testArea.isEmpty()).toBe(true);
+    });
+    it('Returns false if the occupants list is set, the title is defined, and the document is defined', () => {
+      const playerLocation: PlayerLocation = {
+        moving: false,
+        x: 0,
+        y: 0,
+        rotation: 'front',
+      };
+      testArea.occupants = [
+        new PlayerController(nanoid(), nanoid(), playerLocation),
+        new PlayerController(nanoid(), nanoid(), playerLocation),
+        new PlayerController(nanoid(), nanoid(), playerLocation),
+      ];
+      testArea.title = nanoid();
+      testArea.document = nanoid();
+      expect(testArea.isEmpty()).toBe(false);
+    });
   });
   describe('Setting the occupants property', () => {
     it('does not update the property if the new occupants are the same set as the old', () => {
@@ -36,17 +68,20 @@ describe('DocumentAreaController', () => {
       testArea.occupants = shuffledOccupants;
       expect(testArea.occupants).toEqual(origOccupants);
       expect(mockListeners.occupantsChange).not.toBeCalled();
+      expect(mockListeners.titleChange).not.toBeCalled();
     });
     it('emits the occupantsChange event when setting the property and updates the model', () => {
       const newOccupants = testArea.occupants.slice(1);
       testArea.occupants = newOccupants;
       expect(testArea.occupants).toEqual(newOccupants);
       expect(mockListeners.occupantsChange).toBeCalledWith(newOccupants);
+      expect(mockListeners.titleChange).not.toBeCalled();
       expect(testArea.toPresentationAreaModel()).toEqual({
         id: testArea.id,
         occupantsByID: testArea.occupants.map(eachOccupant => eachOccupant.id),
         document: testArea.document,
         slide: testArea.slide,
+        title: testArea.title,
       });
     });
   });
@@ -56,17 +91,20 @@ describe('DocumentAreaController', () => {
       testArea.slide = origSlide;
       expect(testArea.slide).toEqual(origSlide);
       expect(mockListeners.slideChange).not.toBeCalled();
+      expect(mockListeners.titleChange).not.toBeCalled();
     });
     it('emits the changeSlide event when setting the property and updates the model', () => {
       const newSlide = testArea.slide + 1;
       testArea.slide = newSlide;
       expect(testArea.slide).toEqual(newSlide);
       expect(mockListeners.slideChange).toBeCalledWith(newSlide);
+      expect(mockListeners.titleChange).not.toBeCalled();
       expect(testArea.toPresentationAreaModel()).toEqual({
         id: testArea.id,
         occupantsByID: testArea.occupants.map(eachOccupant => eachOccupant.id),
         document: testArea.document,
         slide: testArea.slide,
+        title: testArea.title,
       });
     });
     it('does not emit the changeSlide event when the slide is already 0', () => {
@@ -74,11 +112,13 @@ describe('DocumentAreaController', () => {
       testArea.document = newDocument;
       expect(testArea.document).toEqual(newDocument);
       expect(mockListeners.slideChange).not.toBeCalled();
+      expect(mockListeners.titleChange).not.toBeCalled();
       expect(testArea.toPresentationAreaModel()).toEqual({
         id: testArea.id,
         occupantsByID: testArea.occupants.map(eachOccupant => eachOccupant.id),
         document: testArea.document,
         slide: testArea.slide,
+        title: testArea.title,
       });
     });
     it('does not change the slide when set to a number greater than the number of slides', () => {
@@ -86,11 +126,13 @@ describe('DocumentAreaController', () => {
       testArea.slide = newSlide;
       expect(testArea.slide).toEqual(0);
       expect(mockListeners.slideChange).not.toBeCalled();
+      expect(mockListeners.titleChange).not.toBeCalled();
       expect(testArea.toPresentationAreaModel()).toEqual({
         id: testArea.id,
         occupantsByID: testArea.occupants.map(eachOccupant => eachOccupant.id),
         document: testArea.document,
         slide: testArea.slide,
+        title: testArea.title,
       });
     });
     it('does not change the slide when setting the property to a negative number', () => {
@@ -98,11 +140,13 @@ describe('DocumentAreaController', () => {
       testArea.slide = newSlide;
       expect(testArea.slide).toEqual(0);
       expect(mockListeners.slideChange).not.toBeCalled();
+      expect(mockListeners.titleChange).not.toBeCalled();
       expect(testArea.toPresentationAreaModel()).toEqual({
         id: testArea.id,
         occupantsByID: testArea.occupants.map(eachOccupant => eachOccupant.id),
         document: testArea.document,
         slide: testArea.slide,
+        title: testArea.title,
       });
     });
   });
@@ -112,6 +156,7 @@ describe('DocumentAreaController', () => {
       testArea.document = origDocument;
       expect(testArea.document).toEqual(origDocument);
       expect(mockListeners.slideChange).not.toBeCalled();
+      expect(mockListeners.titleChange).not.toBeCalled();
     });
     it('updates the property and emits documentChange event if the property changes', () => {
       const newDocument = nanoid();
@@ -133,6 +178,7 @@ describe('DocumentAreaController', () => {
         occupantsByID: testArea.occupants.map(eachOccupant => eachOccupant.id),
         document: testArea.document,
         slide: testArea.slide,
+        title: testArea.title,
       });
     });
     it('does not emit the changeSlide event when setting the property to the same document', () => {
@@ -148,6 +194,33 @@ describe('DocumentAreaController', () => {
         occupantsByID: testArea.occupants.map(eachOccupant => eachOccupant.id),
         document: testArea.document,
         slide: testArea.slide,
+        title: testArea.title,
+      });
+    });
+  });
+  describe('Setting the title property', () => {
+    it('does not update the property if the new title is the same as the old', () => {
+      const origTitle = testArea.title;
+      testArea.title = origTitle;
+      expect(testArea.title).toEqual(origTitle);
+      expect(mockListeners.documentChange).not.toBeCalled();
+      expect(mockListeners.slideChange).not.toBeCalled();
+      expect(mockListeners.titleChange).not.toBeCalled();
+    });
+    it('emits the titleChange event when setting the property and updates the model', () => {
+      const newTitle = nanoid();
+      testArea.title = newTitle;
+      expect(testArea.title).toEqual(newTitle);
+      expect(mockListeners.occupantsChange).not.toBeCalled();
+      expect(mockListeners.documentChange).not.toBeCalled();
+      expect(mockListeners.slideChange).not.toBeCalled();
+      expect(mockListeners.titleChange).toBeCalledWith(newTitle);
+      expect(testArea.toPresentationAreaModel()).toEqual({
+        id: testArea.id,
+        occupantsByID: testArea.occupants.map(eachOccupant => eachOccupant.id),
+        document: testArea.document,
+        slide: testArea.slide,
+        title: testArea.title,
       });
     });
   });
@@ -158,6 +231,7 @@ describe('DocumentAreaController', () => {
         occupantsByID: testArea.occupants.map(eachOccupant => eachOccupant.id),
         document: testArea.document,
         slide: testArea.slide,
+        title: testArea.title,
       });
     });
   });
